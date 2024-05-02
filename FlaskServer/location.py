49bc0ef85ae70afb7371279bf163489a3ad8513b
SangYeon Kim <zmdkdkt321@gmail.com>
2024-05-02 23:41:52 +0900

    location csv to db

---
+++ FlaskServer/location.py
@@ -0,0 +1,31 @@
+import csv
+import pymysql
+
+conn = pymysql.connect(host='127.0.0.1', user='dws', password='dws', db='dws', charset='utf8')
+cur = conn.cursor()
+
+f = open('FlaskServer/location.csv','r')
+
+rdr = csv.reader(f)
+ 
+for line in rdr:
+    # if(line[7] == '해당 시도 전체'):
+    #     print(line)
+    #     cur.execute("INSERT INTO LOCATION VALUES("+line[0]+",'"+line[1]+"',"+str(1)+","+str(0)+")")
+    # if(line[7] =='해당 시군구 전체'):
+    #     print(line)
+    #     cur.execute("SELECT id FROM LOCATION WHERE name='"+line[1]+"'")
+    #     result = cur.fetchall()
+    #     print(result[0][0])
+    #     cur.execute("INSERT INTO LOCATION VALUES("+line[0]+",'"+line[2]+"',"+str(2)+","+str(result[0][0])+")")
+    if(line[7] != '해당 시군구 전체' and line[7] != '해당 시도 전체' and line[7] != '법정동(읍면동)'):
+        print(line)
+        cur.execute("SELECT id FROM LOCATION WHERE level IN (1,2) AND name='"+line[2]+"'")
+        result = cur.fetchall()
+        print(result[0][0])
+        cur.execute("INSERT INTO LOCATION VALUES("+line[0]+",'"+line[3]+"',"+str(3)+","+str(result[0][0])+")")
+ 
+f.close()
+
+conn.commit()
+conn.close()
