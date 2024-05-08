4cb61af12208efc5bea2a45d542140871c66b8d8
injunc <injun2366@naver.com>
2024-05-08 14:40:52 +0900

    spring boot server uplaod

---
+++ dsmap/src/main/java/team/men4/dsmap/DsmapApplication.java
@@ -0,0 +1,13 @@
+package team.men4.dsmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmapApplication.class, args);
+	}
+
+}
---
+++ dsmap/src/test/java/team/men4/dsmap/DsmapApplicationTests.java
@@ -0,0 +1,13 @@
+package team.men4.dsmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
