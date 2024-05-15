6909ddc4206219c25d428df7fab2c395a14b48f0
SangYeon Kim <zmdkdkt321@gmail.com>
2024-05-15 11:13:31 +0900

    server complete
    yhaaa

--- FlaskServer/server.py
+++ FlaskServer/server.py
@@ -11,70 +11,42 @@ with open("FlaskServer/config.json", 'r') as f:
 conn = pymysql.connect(host=config["db"]["HOST"], user=config["db"]["user"], password=config["db"]["password"], db='dws', charset='utf8')
 cur = conn.cursor()
 
-# 플라스크 서버의 URL
-url = 'https://www.safetydata.go.kr/V2/api/DSSP-IF-00247'
-
-def readPageNo():
-    with open("FlaskServer/data.json", 'r') as f:
-        data = json.load(f)
-    return data["pageNo"]
-
-def writePageNo(pageNo):
-    with open("FlaskServer/data.json", 'w') as f:
-        data = {
-            "pageNo": pageNo
-        }
-        json.dump(data, f)
-
 def getMessageByPage(pageNo):
-    params = {
-        'serviceKey': '5IF0GM45A509WY1S',
-        'pageNo': pageNo,
-        'numOfRows': '100',
-    }
-    response = requests.get(url, params=params)
+    url = config["api"]["url"]
+    url=url+'?ServiceKey='+config["api"]["serviceKey"]
+    url=url+'&pageNo='+str(pageNo)
+    url=url+'&numOfRows=100'
+    url=url+'&type=json'
+    # print(url)
+    response = requests.get(url)
+    # print(response.url)
     if response.status_code == 200:
         data = json.loads(response.text)
     else:
         print("에러 발생:", response.status_code)
-    return [len(data["body"]),data["body"]]
+    return [len(data["DisasterMsg"][1]["row"]),data["DisasterMsg"][1]["row"]]
 
-# print(getMessageByPage(1)[1])
-
-file_path = "FlaskServer/data.json"
-if not(os.path.exists(file_path)):
-    # 파일이 존재하는 경우 JSON 파일 열기
-    sys.exit("초기화부터 시작")
-
-pageNo = readPageNo()
 
 while True:
-    pageNo = readPageNo()
+    pageNo = 1
     while True:
-        pageCount,data = getMessageByPage(pageNo)
-        print(pageNo)
+        count,data = getMessageByPage(pageNo)
+        print(data[0]["md101_sn"])
+        cur.execute("SELECT count(*) FROM message WHERE id="+data[0]["md101_sn"])
+        result = cur.fetchall()
+        if (result[0][0] == 1): break
+
         for message in data:
-            print("INSERT IGNORE INTO CATEGORY VALUES("+message['DST_SE_ID']+",'"+message['DST_SE_NM']+"')")
-            cur.execute("INSERT IGNORE INTO CATEGORY VALUES("+message['DST_SE_ID']+",'"+message['DST_SE_NM']+"')")
-
-            print("INSERT IGNORE INTO MESSAGE VALUES("+str(message['SN'])+","+
-                        ("true" if message['EMRG_STEP_NM'] == "안전안내" else "false")+","+
-                        message['DST_SE_ID']+",'"+
-                        message['MSG_CN'].replace("\'","")+"','"+
-                        message['CRT_DT']+"')")
-            cur.execute("INSERT IGNORE INTO MESSAGE VALUES("+str(message['SN'])+","+
-                        ("true" if message['EMRG_STEP_NM'] == "안전안내" else "false")+","+
-                        message['DST_SE_ID']+",'"+
-                        message['MSG_CN'].replace("\'","")+"','"+
-                        message['CRT_DT']+"')")
-            for locationID in message['RCPTN_RGN_ID'].split(","):
-                print("INSERT IGNORE INTO MESSAGE_LOCATION VALUES("+str(message['SN'])+",'"+message['DST_SE_NM']+"')")
-                cur.execute("INSERT IGNORE INTO MESSAGE_LOCATION VALUES("+str(message['SN'])+",'"+message['DST_SE_NM']+"')")
-        if(pageCount < 100): break
+            print("INSERT IGNORE INTO MESSAGE VALUES("+message['md101_sn']+",'"+
+                        message['msg']+"','"+
+                        message['create_date']+"')")
+            cur.execute("INSERT IGNORE INTO MESSAGE VALUES("+message['md101_sn']+",'"+
+                        message['msg']+"','"+
+                        message['create_date']+"')")
+            for locationID in message['location_id'].split(","):
+                print("INSERT IGNORE INTO MESSAGE_LOCATION VALUES("+message['md101_sn']+",'"+message['DST_SE_NM']+"')")
+                cur.execute("INSERT IGNORE INTO MESSAGE_LOCATION VALUES("+message['md101_sn']+",'"+message['DST_SE_NM']+"')")
         pageNo+=1
-    writePageNo(pageNo)
-    conn.commit()
+        conn.commit()
     print("wait")
-    time.sleep(60)  # 120초(2분) 동안 대기
-
-conn.close()
+    time.sleep(60)  # 60초 동안 대기
