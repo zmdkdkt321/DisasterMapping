b7f290a8d5d55342b033cbef272794b204c644b6
SangYeon Kim <zmdkdkt321@gmail.com>
2024-05-22 23:23:27 +0900

    chage init location code
    지역 넣는 코드 테이블 구조 변경에 맞춰 수정

--- FlaskServer/location.py
+++ FlaskServer/location.py
@@ -31,15 +31,13 @@ with open("FlaskServer/config.json", 'r') as f:
 conn = pymysql.connect(host=config["db"]["HOST"], user=config["db"]["user"], password=config["db"]["password"], db=config["db"]["db"], charset='utf8')
 cur = conn.cursor()
 
-cur.execute("INSERT IGNORE INTO REGION VALUES("+str(0)+",'"+"전국"+"',"+str(0)+",NULL,NULL,NULL,NULL)")
-
 f = open('FlaskServer/location.csv','r')
 rdr = csv.reader(f)
 for line in rdr:
     if(line[7] == '해당 시도 전체'):
         print(line)
         x,y = getGPSByLocation(line[1])
-        cur.execute("INSERT IGNORE INTO REGION VALUES("+line[0]+",'"+line[1]+"',"+str(1)+","+str(0)+","+line[4]+","+x+","+y+")")
+        cur.execute("INSERT IGNORE INTO REGION VALUES("+line[0]+",'"+line[1]+"','None','None',"+x+","+y+")")
 
 f = open('FlaskServer/location.csv','r')
 rdr = csv.reader(f)
@@ -47,22 +45,17 @@ for line in rdr:
     print("cc")
     if(line[7] =='해당 시군구 전체'):
         print(line)
-        cur.execute("SELECT id FROM REGION WHERE name='"+line[1]+"'")
-        result = cur.fetchall()
-        print(result[0][0])
+        print(line)
         x,y = getGPSByLocation(line[1] + " " + line[2])
-        cur.execute("INSERT IGNORE INTO REGION VALUES("+line[0]+",'"+line[2]+"',"+str(2)+","+str(result[0][0])+","+line[4]+","+x+","+y+")")
+        cur.execute("INSERT IGNORE INTO REGION VALUES("+line[0]+",'"+line[1]+"','"+line[2]+"','None',"+x+","+y+")")
 
 f = open('FlaskServer/location.csv','r')
 rdr = csv.reader(f)
 for line in rdr:
     if(line[7] != '해당 시군구 전체' and line[7] != '해당 시도 전체' and line[7] != '법정동(읍면동)'):
         print(line)
-        cur.execute("SELECT id FROM REGION WHERE level IN (1,2) AND name='"+line[2]+"'")
-        result = cur.fetchall()
-        print(result[0][0])
         x,y = getGPSByLocation(line[1] + " " + line[2] + " " + line[3])
-        cur.execute("INSERT IGNORE INTO REGION VALUES("+line[0]+",'"+line[3]+"',"+str(3)+","+str(result[0][0])+","+line[4]+","+x+","+y+")")
+        cur.execute("INSERT IGNORE INTO REGION VALUES("+line[0]+",'"+line[1]+"','"+line[2]+"','"+line[3]+"',"+x+","+y+")")
  
 f.close()
 
