053bc7164f7fe2b086f0145af9d14c3523a17cdf
HaBoLeum <113341888+HaBoLeum@users.noreply.github.com>
2024-06-12 10:39:57 +0900

    style: menu 고정 및 index.html, msgList.html의 margin, padding 값 변경 client.js의 리스트 상세보기 함수 수정 dsmap.css에서 menu 고정을 위한 함수 추가

--- dsmap/src/main/resources/static/js/mainjs.js
+++ dsmap/src/main/resources/static/js/mainjs.js
@@ -4,32 +4,41 @@ import * as Server from '/js/server.js';
 // 페이지가 로드될 때 실행되는 함수
 
 window.onload = Client.loadMain;
-
 let sseSource = null;
 Server.sseConn(sseSource);
+var infowindow = null;
+var map = null;
+var clusterer = null;
 
+initMap();
 window.addEventListener('beforeunload', function (e) {
     Server.closeSSE(sseSource);
 });
 
-var infowindow = null;
-var map = new kakao.maps.Map(document.getElementById('map'),
- {
-     center: new kakao.maps.LatLng(36.3504, 127.3845),
-     level: 13 // 지도의 초기 확대 레벨
- });
 
 
-var clusterer = new kakao.maps.MarkerClusterer({
-    map: map,
-    averageCenter: true,
-    minLevel: 6
-});
-map.setDraggable(true);
+
+export function initMap(){
+
+    map = new kakao.maps.Map(document.getElementById('map'),
+     {
+         center: new kakao.maps.LatLng(36.3504, 127.3845),
+         level: 13 // 지도의 초기 확대 레벨
+     });
+
+    clusterer = new kakao.maps.MarkerClusterer({
+        map: map,
+        averageCenter: true,
+        minLevel: 6
+    });
+    map.setDraggable(true);
+}
+
+let myList = [];
 
 export function fetchDataAndPlotMarkers() {
 // 데이터 불러오는 부부
-    fetch("/json/dummy.json")
+    fetch("/msg")
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok ' + response.statusText);
@@ -45,8 +54,17 @@ export function fetchDataAndPlotMarkers() {
             }
 
 
+
           var x = localStorage.getItem('x');
           var y = localStorage.getItem('y');
+
+          var imageSrc = 'http://localhost:8080/img/green', // 마커이미지의 주소입니다
+              imageSize = new kakao.maps.Size(28, 32), // 마커이미지의 크기입니다
+              imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+          // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+          var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
 
           if (x !== null && y !== null) {
                var mp  = new kakao.maps.LatLng(x, y);
@@ -63,8 +81,11 @@ export function fetchDataAndPlotMarkers() {
                 var markerPosition = new kakao.maps.LatLng(parseFloat(item.x), parseFloat(item.y));
                 var marker = new kakao.maps.Marker({
                     map: map,
-                    position: markerPosition
+                    position: markerPosition,
+                    image: markerImage
                 });
+
+                myList.push(marker);
                 clusterer.addMarker(marker);
 
                 // 마커에 이벤트 리스너 등록
@@ -73,6 +94,7 @@ export function fetchDataAndPlotMarkers() {
                 });
 
                 // 마우스 호버 이벤트
+                // TODO [javascript] 해당 하는 폴리건 표시
                 kakao.maps.event.addListener(marker, 'mouseover', function() {
                     displayAreaInfo(markerPosition, item, infowindow);
                 });
@@ -104,6 +126,12 @@ export function displayAreaInfo(coords, messages) {
 export function loadMap() { //main에 지도 페이지 비동기 연결
 //            mapMsgListJson(); //초기 리스트 생성
 //            mapMake()//카카오맵 생성 및 클러스트, 마커 생성
+
+    clusterer.clear();
+    myList.forEach(function(obj) {
+        obj.setMap(null);
+    });
+
     const container = document.getElementById("content");
             // 가져온 데이터를 해당 div에 추가
     container.innerHTML = "";
